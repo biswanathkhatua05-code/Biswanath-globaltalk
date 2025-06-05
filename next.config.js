976ac8ca96f190e461e5215b62311de4a91a2bfa
Cis Kuch bhi nehi <biswanathkhatua05@gmail.com>
2025-06-05 05:53:57 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

---
+++ next.config.js
@@ -0,0 +1,25 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  experimental: {
+    allowedDevOrigins: ['https://6000-firebase-studio-1747198792422.cluster-iktsryn7xnhpexlu6255bftka4.cloudworkstations.dev'],
+  },
+};
+
+module.exports = nextConfig;
