feac4df067a116ea6ad7f3d824995ed39854d45d
Cis Kuch bhi nehi <biswanathkhatua05@gmail.com>
2025-06-06 05:05:35 +0000

    Eh show ho raha hai terminal me

--- next.config.js
+++ next.config.js
@@ -3,7 +3,9 @@ const nextConfig = {
   experimental: {
     allowedDevOrigins: [
       "https://3001-firebase-studio-1747018792422.cluster-iktsryn7.xnhpexlu6255bftka4.cloudworkstations.dev",
-      "https://6000-firebase-studio-1747198792422.cluster-iktsryn7xnhpexlu6255bftka4.cloudworkstations.dev"
+      "https://6000-firebase-studio-1747198792422.cluster-iktsryn7xnhpexlu6255bftka4.cloudworkstations.dev",
+      "https://3000-firebase-studio-1747018792422.cluster-iktsryn7.xnhpexlu6255bftka4.cloudworkstations.dev", // Example, if you also use port 3000
+      "https://9003-firebase-studio-1747018792422.cluster-iktsryn7.xnhpexlu6255bftka4.cloudworkstations.dev", // Added for new port
     ],
   },
 };
