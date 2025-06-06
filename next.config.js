8838d99315e90db75401bdb0934b066193838f40
Cis Kuch bhi nehi <biswanathkhatua05@gmail.com>
2025-06-06 01:40:53 +0000

    Rebuild Next.js plz

--- next.config.js
+++ next.config.js
@@ -1,24 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
   experimental: {
-    allowedDevOrigins: ['https://6000-firebase-studio-1747198792422.cluster-iktsryn7xnhpexlu6255bftka4.cloudworkstations.dev'],
+    allowedDevOrigins: [
+      "https://3001-firebase-studio-1747018792422.cluster-iktsryn7.xnhpexlu6255bftka4.cloudworkstations.dev",
+      "https://6000-firebase-studio-1747198792422.cluster-iktsryn7xnhpexlu6255bftka4.cloudworkstations.dev"
+    ],
   },
 };
 
