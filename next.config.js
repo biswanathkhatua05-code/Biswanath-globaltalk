abcb2558e467fcf971c01007afa2f1f015c97ce3
Cis Kuch bhi nehi <biswanathkhatua05@gmail.com>
2025-08-04 10:56:34 +0000

    But maine toh koi host nhi ki hai or mujhe eh dikha Raha hai , iska matl

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ const nextConfig = {
       "https://6000-firebase-studio-1747198792422.cluster-iktsryn7xnhpexlu6255bftka4.cloudworkstations.dev",
       "https://3000-firebase-studio-1747018792422.cluster-iktsryn7.xnhpexlu6255bftka4.cloudworkstations.dev", // Example, if you also use port 3000
       "https://9003-firebase-studio-1747198792422.cluster-iktsryn7xnhpexlu6255bftka4.cloudworkstations.dev", // Added for new port
+      "https://9004-firebase-studio-1747198792422.cluster-iktsryn7xnhpexlu6255bftka4.cloudworkstations.dev" // Added to fix cross-origin error
     ],
   },
   images: {
